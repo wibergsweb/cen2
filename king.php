c6aa3e95d5978f4bafb32b246542b12eae5cf2da
Gustav Wiberg <gustav.wiberg@ntig.se>
2019-11-07 09:38:42 +0100

    Removal of chess check inside of king class. This is checked for inside of game class

--- king.php
+++ king.php
@@ -20,11 +20,8 @@ class King extends Piece {
                 $vm = array($xd,$yd);
             }
             else if ($check_piece !== null && $this->get_color() != $check_piece->get_color()) {
-                    $vm = array($xd, $yd);
+                $vm = array($xd, $yd);
             }
-
-
-            
         }
         
         return $vm;
@@ -41,56 +38,13 @@ class King extends Piece {
         $valid_moves[] = $this->check($gridpositions,$x,$y,-1,1);       //check down left
         $valid_moves[] = $this->check($gridpositions,$x,$y,1,1);        //check down right      
         
-        //Is king chess when king has moved?
+        //Valid moves for the other king is NOT valid moves for this king
+        //(Make sure kings cannot stand besides eachother)
         $king = $gridpositions[$x][$y];
         $fake_gridpositions = array_slice($gridpositions,0,count($gridpositions));        
         $fake_gridpositions[$x2][$y2] = $king; //Check if valid now when king is on new position
         $fake_gridpositions[$x][$y] = null; //Make sure not check this gridposition
 
-        //Go through whole board and check if some piece is
-        //checking the king on the new position
-        for($yp=0;$yp<8;$yp++) {
-            for($xp=0;$xp<8;$xp++) {
-                $piece = $fake_gridpositions[$xp][$yp];
-                if ($piece !== null && !$piece instanceof King) {
-
-                    //Get valid moves for each piece on board and check
-                    //if any piece is checking this king
-                    $validmoves_piece = $piece->get_validmoves($fake_gridpositions, $xp, $yp, 0,0);
-                    
-                    //Only check more if there are any valid moves piece
-                    //in current fake gridpositions
-                    if (!empty($validmoves_piece)) {
-                        $aftermove = $piece->get_aftermove($fake_gridpositions,$xp,$yp);
-                        if (!empty($aftermove)) {
-                            if (stristr($aftermove[1],'chess') !== false) {                                
-                                //IF above position (Where piece checks king) is included
-                                //in valid moves, then remove this validity
-                                //$x2 and $y2 is the king's position in original grid
-                                foreach($valid_moves as $vm_key => $vm) {
-                                    if (!empty($vm)) {
-                                        $vmx = $vm[0];
-                                        $vmy = $vm[1];
-
-                                        if ($x2 == $vmx && $y2 == $vmy) {
-                                            unset($valid_moves[$vm_key]);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                    }
-
-
-                }
-            }
-        }
-        //End Is king chess when king has moved?
-        
-        
-        //Valid moves for the other king is NOT valid moves for this king
-        //(Make sure kings cannot stand besides eachother)
         for($yp=0;$yp<8;$yp++) {
             for($xp=0;$xp<8;$xp++) {
                 $piece = $fake_gridpositions[$xp][$yp];
